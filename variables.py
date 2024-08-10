3f423314a1357e77d9d736967187f49641061c62
PySaif01 <Dayforme90@gmail.com>
2024-08-10 14:13:15 +0300

    variables.py

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
-Homeworks_count = '12'
-Hours_spent = '1.5'
-Course_name = 'Python'
-time_for_one_task = str(1.5 / 12)
-print('Курс: '+Course_name+', всего задач: '+Homeworks_count+', затрачено часов: '+Hours_spent+' ,среднее время выполнениея: '+time_for_one_task)
+homeworks_count = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = hours_spent / homeworks_count
+print('Курс: ',course_name,', всего задач: ',homeworks_count,', затрачено часов: ',hours_spent,' , среднее время выполнениея: ',time_for_one_task)
