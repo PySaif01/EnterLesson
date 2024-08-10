9132b8f4ecee98f53dbba11433fa87c7efa2ed26
PySaif01 <Dayforme90@gmail.com>
2024-08-10 11:51:53 +0300

    Add files via upload

---
+++ String index.py
@@ -0,0 +1,6 @@
+example = "Типография"
+print(example[0])
+print(example[-1])
+print(example[5:10])
+print(example[::-1])
+print(example[1::2])
---
+++ variables.py
@@ -0,0 +1,5 @@
+Homeworks_count = '12'
+Hours_spent = '1.5'
+Course_name = 'Python'
+time_for_one_task = str(1.5 / 12)
+print('Курс: '+Course_name+', всего задач: '+Homeworks_count+', затрачено часов: '+Hours_spent+' ,среднее время выполнениея: '+time_for_one_task)
